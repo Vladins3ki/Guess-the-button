e0c2e7074537174ee9e0dca2b3926463a540539d
Vladins3ki <123756024+Vladins3ki@users.noreply.github.com>
2023-01-30 15:39:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+let randomNumber = Math.floor(Math.random() * 3) +1;
 
-let x = Math.floor(Math.random() * 3) +1;
+document.getElementById(randomNumber).value = "You clicked the right button!";
 
-document.getElementById(x).value = "CORRECT!";
-
-$("button").click(function() {
-    let fired_button = $(this).val();
-    document.getElementById("format").innerHTML = fired_button;
-     
-});
+function apasa(value){
+    document.getElementById("format").innerHTML = value;
+}
 
 function reset(){
-    document.getElementById("restButton").innerHTML = window.location.reload();
+     window.location.reload();
 }
 
 
